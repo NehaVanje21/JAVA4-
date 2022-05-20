b9b4f26b04a7925503223a20c8f59d7f48453ca4
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-05-20 10:07:31 +0530

    Add files via upload

---
+++ Praticals/RelationalOperator.java
@@ -0,0 +1,19 @@
+package Neha;
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num1 = 14, num2 = 6;
+		
+		System.out.println("num1 == num2 = " + (num1 == num2) );
+		System.out.println("num1 != num2 = " + (num1 != num2) );
+		System.out.println("num1 >  num2 = " + (num1 >  num2) );
+		System.out.println("num1 <  num2 = " + (num1 <  num2) );
+		System.out.println("num1 >= num2 = " + (num1 >= num2) );
+		System.out.println("num1 <= num2 = " + (num1 <= num2) );
+
+
+	}
+
+}
