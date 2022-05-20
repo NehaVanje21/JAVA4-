e44ef309c4ee3992fcfe4a2c75d4f6e52db65167
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-05-20 17:14:48 +0530

    Add files via upload

---
+++ Praticals/Continue.java
@@ -0,0 +1,21 @@
+package Neha;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int j=1; j<=7; j++)
+		{
+		if (j==3)
+		{
+		continue;
+		}
+		System.out.print(j+" ");
+		}
+		}
+		}
+
+
+	
+
+
