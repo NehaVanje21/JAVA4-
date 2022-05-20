0e7f0d32c802079768cef4c956bd76cf93189b77
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-05-20 10:05:06 +0530

    Add files via upload

---
+++ Praticals/LongDataType.java
@@ -0,0 +1,19 @@
+package Neha;
+
+public class LongDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long myLong1, myLong2, result;
+		myLong1 = 100003456L;
+		myLong2 = 223L;
+		result = myLong1 * myLong2;
+		System.out.println("Number 1: " +myLong1);
+		System.out.println("Number 2: " +myLong2);
+		System.out.println("Number 1 * Number 2: " +result);
+		}
+		}
+
+
+
+
