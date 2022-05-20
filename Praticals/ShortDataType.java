3e57c00b6f5ec714337242df378b600fb95c07d1
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-05-20 10:08:43 +0530

    Add files via upload

---
+++ Praticals/ShortDataType.java
@@ -0,0 +1,12 @@
+package Neha;
+
+public class ShortDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short myShort = 5000;
+		System.out.println("myShort: " + myShort);
+		}
+	}
+
+	
