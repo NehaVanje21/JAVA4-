8feba9733535fe41448a05d1c1f8246fec761570
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-05-20 10:01:15 +0530

    Add files via upload

---
+++ Praticals/Increment.java
@@ -0,0 +1,14 @@
+package Neha;
+
+public class Increment {
+	public static void main(String[] args) {
+		int p = 4; int q = 3; int r;
+		 
+		int s;
+		r = ++q; s = p++; r++;
+
+		System.out.println("p = " + p + " q = " + q + " r = " + r + " s = " + s);
+
+
+	}
+	}
