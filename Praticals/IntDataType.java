895cd7c6311b91bc60fa6a85408b8ede1f2601a9
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-05-20 10:02:53 +0530

    Add files via upload

---
+++ Praticals/IntDataType.java
@@ -0,0 +1,16 @@
+package Neha;
+
+public class IntDataType {
+	public static void main(String args[])
+	{
+	int myNum1, myNum2, result;
+	myNum1 = -7019;
+	myNum2 = 90182;
+	result = myNum1 + myNum2;
+	System.out.println("Number 1: " +myNum1);
+	System.out.println("Number 2: " +myNum2);
+	System.out.println("Number 1 + Number 2: " +result);
+	}
+	}
+
+
