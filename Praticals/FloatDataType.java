b5e6baaf6bab43a36aa8e92961614289bf77010c
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-05-20 09:58:10 +0530

    Add files via upload

---
+++ Praticals/FloatDataType.java
@@ -0,0 +1,19 @@
+package Neha;
+
+public class FloatDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		float myFloat1,myFloat2,result;
+		myFloat1=1000.656f;
+		myFloat2=120.72f;
+		result=myFloat1-myFloat2;
+		System.out.println("Number1: "+myFloat1);
+		System.out.println("Number2: "+myFloat2);
+		System.out.println("Number1-Number2: "+result);
+
+	}
+
+}
