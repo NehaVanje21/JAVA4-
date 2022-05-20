3289f6f5458296ff991b3afc066b45deddf39301
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-05-20 15:29:35 +0530

    Add files via upload

---
+++ Praticals/Widening.java
@@ -0,0 +1,14 @@
+package Neha;
+
+public class Widening {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b = 70;
+		int c = (int)(b * 2);
+		System.out.println(c);
+		
+
+	}
+
+}
