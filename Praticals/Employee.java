8d8efa0a6e42afd3ae74ea7d2c32222bc13108fe
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-05-20 09:55:35 +0530

    Add files via upload

---
+++ Praticals/Employee.java
@@ -0,0 +1,32 @@
+package Neha;
+
+public class Employee {
+
+	int empId;
+	String empName;
+	Employee(int id,String name){
+		
+		this.empId=id;
+		this.empName=name;
+	}
+	void info() {
+		System.out.println("Id:"+empId+"Name:"+empName);
+		
+	}
+	public static void main(String args[]) {
+		Employee obj1=new Employee(100,"Neha");
+		Employee obj2=new Employee(109,"Pallavi");
+		obj1.info();
+		obj2.info();
+	}
+}
+	
+	
+	
+	
+
+
+
+		
+
+
