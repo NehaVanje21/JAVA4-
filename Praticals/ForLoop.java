dc7c4b4e14ab69cf0e1483ee34ed78a70a763cee
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-05-20 17:12:11 +0530

    Add files via upload

---
+++ Praticals/ForLoop.java
@@ -0,0 +1,14 @@
+package Neha;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=5;i>2; i--)
+		{
+			System.out.println("The value of i is: "+i);
+
+	}
+
+}
+}
