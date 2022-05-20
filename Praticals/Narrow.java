bda396b78b255eb78e39a99b8f81529e91937080
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-05-20 15:16:54 +0530

    Add files via upload

---
+++ Praticals/Narrow.java
@@ -0,0 +1,25 @@
+package Neha;
+
+public class Narrow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			double d = 212.06;
+			 
+			//explicit type casting
+			long l = (long)d;
+			 
+			//explicit type casting
+			int i = (int)l;
+			System.out.println("Double Data type value "+d);
+			 
+			//fractional part lost
+			System.out.println("Long Data type value "+l);
+			 
+			//fractional part lost
+			System.out.println("Int Data type value "+i);
+			}
+			}
+
+
