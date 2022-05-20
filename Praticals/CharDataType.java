c3fe02b01b1924b42eaeee57d0ac405de42fe430
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-05-20 09:52:22 +0530

    Add files via upload

---
+++ Praticals/CharDataType.java
@@ -0,0 +1,18 @@
+package Neha;
+
+public class CharDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char marks, grade; // declare char datatype variable
+		// initialize value to variable
+		marks = '9';
+		grade = 'A';
+		// printing on console
+		System.out.println("Marks:"+marks);
+		System.out.println("Grade: "+grade);
+
+
+	}
+
+}
