b86b3b4e68d2da937f05e8af64f074f157d4dff7
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-05-20 17:16:22 +0530

    Add files via upload

---
+++ Praticals/DoWhileLoop.java
@@ -0,0 +1,18 @@
+package Neha;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=7;
+		do{
+		System.out.println(i);
+		i--;
+		}while(i>3);
+		}
+		}
+
+
+	
+
+
