dd317222c40a35add7c89049e9235980b3077ba4
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-05-20 09:48:30 +0530

    Add files via upload

---
+++ Praticals/ArithemeticOperator.java
@@ -0,0 +1,20 @@
+package Neha;
+
+public class ArithemeticOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+			
+			int a=21; 
+			int b=11;
+			
+		System.out.println(a+b); 
+		System.out.println(a-b); 
+		System.out.println(a*b); 
+		System.out.println(a/b); 
+		System.out.println(a%b);
+		
+
+	}
+
+}
