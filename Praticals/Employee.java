3146b91924eeebef40ca7a30c601307033ab0b41
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-06-01 15:51:15 +0530

    Add files via upload

--- Praticals/Employee.java
+++ Praticals/Employee.java
@@ -1,32 +1,43 @@
 package Neha;
 
 public class Employee {
+	
 
-	int empId;
-	String empName;
-	Employee(int id,String name){
+
+		int emp_id;
+		String emp_name;
+		double emp_sal;
+		long emp_ph;
 		
-		this.empId=id;
-		this.empName=name;
-	}
-	void info() {
-		System.out.println("Id:"+empId+"Name:"+empName);
+		void show() {
+			System.out.println("Employee details are: ");
+		}
 		
+		void display(int i,String n,double s,long p) {
+			emp_id=i;
+			emp_name=n;
+			emp_sal=s;
+			emp_ph=p;
+			
+			System.out.println(emp_id + " " + emp_name + " " + emp_sal + " " + emp_ph);
+			
+			
+		}
+
+		public static void main(String[] args) {
+			Employee e= new Employee();
+			Employee e1= new Employee();
+			Employee e2= new Employee();
+			
+			e.show();
+			e.display(110,"Mayuri",23123.45,900667457);
+			e1.display(112,"Neha",12453.3,796534456);
+			e2.display(113,"Purva",184500.23,835756757);
+			
+			
+
+		}
+
 	}
-	public static void main(String args[]) {
-		Employee obj1=new Employee(100,"Neha");
-		Employee obj2=new Employee(109,"Pallavi");
-		obj1.info();
-		obj2.info();
-	}
-}
-	
-	
-	
-	
-
-
-
-		
 
 
