736e6e1f600054152896faf61503e7d306cc26c7
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-06-01 16:07:28 +0530

    Add files via upload

---
+++ Praticals/BreakExample.java
@@ -0,0 +1,19 @@
+package Neha;
+
+public class BreakExample {
+	
+
+		public static void main(String[] args)
+		{
+			for(int i=2;i<=8;i++)
+			{
+				if(i==6)
+				{
+					break;
+				}
+				System.out.println(i);
+			}
+		}
+
+	}
+
