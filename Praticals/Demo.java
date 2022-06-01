87c64d047b0164b0af6dcb3e8719ea09c4b04be1
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-06-01 16:03:22 +0530

    Add files via upload

---
+++ Praticals/Demo.java
@@ -0,0 +1,15 @@
+package Neha;
+
+public class Demo {
+	
+		public Demo()
+		{
+			System.out.println("Constructor is no argument type.");
+		}
+
+		public static void main(String[] args) {
+			new Demo();
+		}
+
+	}
+
