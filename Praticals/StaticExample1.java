a431352a595c6fc16e99a027395c35f14619b466
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-06-06 18:03:46 +0530

    Add files via upload

---
+++ Praticals/StaticExample1.java
@@ -0,0 +1,27 @@
+package Neha;
+
+public class StaticExample1 {
+	
+		
+		//instance variable
+		int number=101;
+		
+		//creating static method
+		static void display() {
+			String name="Neha";
+			System.out.println("Name is : " +name);
+		}
+
+		public static void main(String[] args) {
+			
+			//creating object
+			StaticExample1 s= new StaticExample1();
+			
+			System.out.println("Number is :" +s.number); //calling variable to display values
+			StaticExample1.display(); //calling static method
+
+		}
+
+	}
+
+
