018594fede152b4d95e6174556f136313345f3be
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-07-12 16:26:31 +0530

    Add files via upload

---
+++ Praticals/ArrayEx1.java
@@ -0,0 +1,57 @@
+package Neha;
+
+public class ArrayEx1 {
+	 
+	public static void main(String[] args)
+	{
+		//datatype arrayname=new datatype size
+		int arr[]=new int[10];      //declaration and instantiation
+		
+
+		arr[0]=100;   //initialization
+		arr[1]=102;
+		arr[2]=103;
+		arr[3]=104;
+		arr[4]=105;
+		arr[5]=106;
+		arr[6]=107;
+		arr[7]=108;
+		arr[8]=109;
+		arr[9]=110;
+		//arr[10]=111:   //inserting element outside the bound
+		
+		//traversing array
+		for(int i=0;i<arr.length;i++)  //length is property of array
+		{
+			System.out.print("Array value are : "+ arr[i]+"");
+			
+			
+		}
+		System.out.println();
+		System.out.println("Array Value are:"+ arr[6]+" ");
+		System.out.println(arr.length);
+		
+		String ar[]= {"aa","bb","cc","dd","ee","ff"	};
+		
+		//traversing array
+		//for (int i=0;i<ar.length;i++)
+			//System.out.println(ar[i] + " ");
+	//}
+	
+	//printing using for-each loop
+	for (String my :ar)
+	{
+		System.out.print(my + " ");
+	}
+}
+}
+
+
+
+		
+		
+		
+		
+
+	
+
