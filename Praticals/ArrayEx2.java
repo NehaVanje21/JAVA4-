283c5f2ab81b7d1f2c2987f2efcd2ddf9389838d
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-07-12 16:52:20 +0530

    Add files via upload

---
+++ Praticals/ArrayEx2.java
@@ -0,0 +1,20 @@
+package Neha;
+
+public class ArrayEx2 {
+	
+	public void display(int arr[]) {
+		
+	for(int i=0;i<arr.length;i++)
+	{
+		System.out.println(arr[i] + " ");
+		
+	}
+
+}
+	public static void main(String[] args) {
+		ArrayEx2 a = new ArrayEx2();
+		int arr[]= {1,2,3,4,5,6,7,8};
+		a.display(arr);
+	}
+}
+			
