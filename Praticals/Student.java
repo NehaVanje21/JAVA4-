a3d170d5d8cc1b484e6938d01da56f3896192fa2
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-07-12 14:33:51 +0530

    Add files via upload

--- Praticals/Student.java
+++ Praticals/Student.java
@@ -1,30 +1,44 @@
 package Neha;
 
-public class Student {
-	 int id;
-	   String name;
-	   
-	  int age;
-	   Student(int i,String n){
-	 id = i;
-	  name = n;
-	   }
-	   
-	   Student(int i,String n,int a){
-	  id = i;
-	  name = n;
-	  age = a;
-	   }
-	   
-	   void display(){System.out.println(id+" "+name+" "+age);}
-	   
-	   public static void main(String args[]) {
-		   
-		   Student s1 = new Student(16,"Ali");
-		   Student s2 = new Student(22,"ele",50);
-		   s1.display();
-		   s2.display();
-	   }
-	   
-}
+
+	public class Student {
+
+		//declaring variables as private 
+		private int id;
+		private String sName;
+		private String SEmail;
+		private long sPhone;
+
+		//getter and setter methods for accessing private variables
+
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getsName() {
+			return sName;
+		}
+		public void setsName(String sName) {
+			this.sName = sName;
+		}
+		public String getSEmail() {
+			return SEmail;
+		}
+		public void setSEmail(String sEmail) {
+			SEmail = sEmail;
+		}
+		public long getsPhone() {
+			return sPhone;
+		}
+		public void setsPhone(long sPhone) {
+			this.sPhone = sPhone;
+		}
+
+
+
+
+
+	}
 
