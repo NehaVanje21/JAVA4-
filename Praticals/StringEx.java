8daef1cdd5ce97492e4993b92f6f14a1def85046
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-07-15 16:36:52 +0530

    Add files via upload

---
+++ Praticals/StringEx.java
@@ -0,0 +1,31 @@
+package Neha;
+
+public class StringEx 
+{
+	public static void main(String[] args)
+	{
+	
+	String s ="Hello";//creating string using literal 
+	String s1="Hello";
+	System.out.println(s);
+	System.out.println(s1);
+	//or
+	char[]arr = {'H','e','l','l','o'};
+	String S11=new String(arr); //converting char array to string
+	System.out.println(S11);
+	//or
+	String s3=new String ("World");//creating string using new keyword
+	String s4=new String ("World");
+	System.out.println(s3);
+	System.out.println(s4);
+}
+}
+	
+	
+	
+	
+	
+	
+	
+
+
